c238a4f415ed6330c8a2c8c3338f4f3d3d253972
Lance Velasco <lance@mlvgroup.com>
2013-08-27 07:59:12 -0700

    Download JSON Records to file

--- GolfCalculator/MLVSyncEngine.m
+++ GolfCalculator/MLVSyncEngine.m
@@ -109,8 +109,8 @@
          {
              NSDictionary * responseObject = (NSDictionary * )JSON;
              if ([responseObject isKindOfClass:[NSDictionary class]]) {
-                 NSLog(@"Response for %@: %@", className, responseObject);
-//                 [self writeJSONResponse:responseObject toDiskForClassWithName:className];
+//                 NSLog(@"Response for %@: %@", className, responseObject);
+                 [self writeJSONResponse:responseObject toDiskForClassWithName:className];
              } else {
                  NSLog(@"response not Json");
              }
@@ -132,6 +132,47 @@
     }];
 }
 
+#pragma mark - File Management
 
+- (NSURL *)applicationCacheDirectory
+{
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSCachesDirectory inDomains:NSUserDomainMask] lastObject];
+}
 
+- (NSURL *)JSONDataRecordsDirectory{
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSURL *url = [NSURL URLWithString:@"JSONRecords/" relativeToURL:[self applicationCacheDirectory]];
+    NSError *error = nil;
+    if (![fileManager fileExistsAtPath:[url path]]) {
+        [fileManager createDirectoryAtPath:[url path] withIntermediateDirectories:YES attributes:nil error:&error];
+    }
+    
+    return url;
+}
+
+- (void)writeJSONResponse:(id)response toDiskForClassWithName:(NSString *)className {
+    NSURL *fileURL = [NSURL URLWithString:className relativeToURL:[self JSONDataRecordsDirectory]];
+    if (![(NSDictionary *)response writeToFile:[fileURL path] atomically:YES]) {
+        NSLog(@"Error saving response to disk, will attempt to remove NSNull values and try again.");
+        // remove NSNulls and try again...
+        NSArray *records = [response objectForKey:@"results"];
+        NSMutableArray *nullFreeRecords = [NSMutableArray array];
+        for (NSDictionary *record in records) {
+            NSMutableDictionary *nullFreeRecord = [NSMutableDictionary dictionaryWithDictionary:record];
+            [record enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+                if ([obj isKindOfClass:[NSNull class]]) {
+                    [nullFreeRecord setValue:nil forKey:key];
+                }
+            }];
+            [nullFreeRecords addObject:nullFreeRecord];
+        }
+        
+        NSDictionary *nullFreeDictionary = [NSDictionary dictionaryWithObject:nullFreeRecords forKey:@"results"];
+        
+        if (![nullFreeDictionary writeToFile:[fileURL path] atomically:YES]) {
+            NSLog(@"Failed all attempts to save response to disk: %@", response);
+        }
+    }
+}
 @end
